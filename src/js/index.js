93f81e7a488b2fa93feace191571a7742e7426db
Arhant Jain <arhantjain2@gmail.com>
2021-02-22 02:28:44 +0530

    Added flash-messages

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Build main.scss along
 import '../scss/main.scss';
 
+import './component-functions/flash-messages';
 import './component-functions/sidebar';
 import './component-functions/sliders';
 import './component-functions/increment-decrement-input-number';
